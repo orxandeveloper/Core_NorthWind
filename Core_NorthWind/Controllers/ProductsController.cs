bd4929add08d9466783aa809a56b541ae64a1525
orxandeveloper <43696146+orxandeveloper@users.noreply.github.com>
2022-03-04 11:27:34 +0300

    Add project files.

---
+++ Core_NorthWind/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nw.Infra.Context;
+using System.Threading.Tasks;
+
+namespace Core_NorthWind.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly NwContext nwContext;
+        public ProductsController(NwContext nwContext_)
+        {
+            nwContext = nwContext_;
+        }
+        public async Task<IActionResult> ProductsRegister()
+        {
+            var northwindContext = nwContext.Products.Include(p=>p.Category).Include(p=>p.Supplier).ToListAsync();    
+
+            return View( await northwindContext);
+        }    
+    }
+}
---
+++ Nw.Domain/Entities/Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Core.Domain.Entities
+{
+    public class Products : IEntity
+    {
+        public Products()
+        {
+            OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int ProductId { get; set; }
+        [Required (ErrorMessage ="Product Name can not be null or empty")]
+        public string ProductName { get; set; }
+        public int? SupplierId { get; set; }
+        public int? CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        public Categories Category { get; set; }
+        public Suppliers Supplier { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+       // public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Id { get; set; } = 1;
+    }
+}
